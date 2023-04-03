ca7b45356555d282e36c142ef754936b0c22d6e7
kimura.hiroaki <hmkimura.webcreate@gmail.com>
2023-04-03 23:35:36 +0900

    <add>Modify High day12 practice wordpress .

--- tf30/index.php
+++ tf30/index.php
@@ -56,46 +56,47 @@
                 <!-- entries -->
                 <div class="entries">
                     <?php if (have_posts()) : ?>
-                        <?php while (have_posts()) : ?>
-                            <?php the_post(); ?>
-                            <!-- entry-item -->
-                            <a href="<?php the_permalink(); ?>" class="entry-item">
-                                <!-- entry-item-img -->
-                                <div class="entry-item-img">
-                                    <?php if (has_post_thumbnail()) : ?>
-                                        <?php the_post_thumbnail(); ?>
-                                    <?php else : ?>
-                                        <img src="<?php echo get_template_directory_uri(); ?>/img/noimg.png" alt="">
-                                    <?php endif; ?>
-                                </div><!-- /entry-item-img -->
+                    <?php while (have_posts()) : ?>
+                    <?php the_post(); ?>
+                    <!-- entry-item -->
+                    <a href="<?php the_permalink(); ?>" class="entry-item">
+                        <!-- entry-item-img -->
+                        <div class="entry-item-img">
+                            <?php if (has_post_thumbnail()) : ?>
+                            <?php the_post_thumbnail(); ?>
+                            <?php else : ?>
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/noimg.png" alt="">
+                            <?php endif; ?>
+                        </div><!-- /entry-item-img -->
 
-                                <!-- entry-item-body -->
-                                <div class="entry-item-body">
-                                    <div class="entry-item-meta">
-                                        <?php
+                        <!-- entry-item-body -->
+                        <div class="entry-item-body">
+                            <div class="entry-item-meta">
+                                <?php
                                         $category = get_the_category(); ?>
-                                        <?php
+                                <?php
                                         if ($category[0]) : ?>
-                                            <div class="entry-item-tag"><?php echo $category[0]->cat_name; ?></div>
-                                            <!-- /entry-item-tag -->
-                                        <?php endif ?>
-                                        <time class="entry-item-published" datetime="<?php the_time("c") ?>"><?php the_time("y/n/j") ?></time>
-                                        <!-- /entry-item-published -->
-                                    </div><!-- /entry-item-meta -->
-                                    <h2 class="entry-item-title"><?php the_title(); ?></h2><!-- /entry-item-title -->
-                                    <div class="entry-item-excerpt">
-                                        <p><?php the_excerpt(); ?></p>
-                                    </div><!-- /entry-item-excerpt -->
-                                </div><!-- /entry-item-body -->
-                            </a><!-- /entry-item -->
-                        <?php endwhile; ?>
+                                <div class="entry-item-tag"><?php echo $category[0]->cat_name; ?></div>
+                                <!-- /entry-item-tag -->
+                                <?php endif ?>
+                                <time class="entry-item-published"
+                                    datetime="<?php the_time("c") ?>"><?php the_time("y/n/j") ?></time>
+                                <!-- /entry-item-published -->
+                            </div><!-- /entry-item-meta -->
+                            <h2 class="entry-item-title"><?php the_title(); ?></h2><!-- /entry-item-title -->
+                            <div class="entry-item-excerpt">
+                                <p><?php the_excerpt(); ?></p>
+                            </div><!-- /entry-item-excerpt -->
+                        </div><!-- /entry-item-body -->
+                    </a><!-- /entry-item -->
+                    <?php endwhile; ?>
                     <?php endif; ?>
                 </div><!-- /entries -->
 
                 <?php if (paginate_links()) : ?>
-                    <!-- pagination -->
-                    <div class="pagination">
-                        <?php
+                <!-- pagination -->
+                <div class="pagination">
+                    <?php
                         echo paginate_links(
                             array(
                                 "end_size" => 1,
@@ -106,7 +107,7 @@
                             )
                         )
                         ?>
-                    </div><!-- /pagination -->
+                </div><!-- /pagination -->
                 <?php endif; ?>
 
             </main><!-- /primary -->
